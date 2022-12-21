95d5d7969d850fe978cb35359f05e8adc078d3e3
VPavliashvili <v_pavliashvili@yahoo.com>
2022-12-21 23:15:22 +0400

    redundant folders and code removed

--- domain/errors.go
+++ domain/errors.go
@@ -18,3 +18,19 @@ func (err NonExistentCommandError) Is(target error) bool {
 		panic("only NonExistentCommandError error is expected")
 	}
 }
+
+type InvalidPathError struct {
+	Path string
+}
+
+func (i InvalidPathError) Error() string {
+	return fmt.Sprintf("file -> %v does not exist", i.Path)
+}
+
+type NotPictureError struct {
+	File string
+}
+
+func (i NotPictureError) Error() string {
+	return fmt.Sprintf("File -> %v is not an image", i.File)
+}
