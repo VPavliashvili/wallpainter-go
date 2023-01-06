ec929af690282b785afffb388d7328cccab7e085
VPavliashvili <v_pavliashvili@yahoo.com>
2023-01-07 03:54:04 +0400

    implementation of slideshow command started, currently writing opts parsing

--- args/parser/slideshowopts/slideshowopts.go
+++ args/parser/slideshowopts/slideshowopts.go
@@ -1,1 +1,24 @@
 package slideshowopts
+
+import (
+	options "github.com/VPavliashvili/wallpainter-go/domain/opts"
+)
+
+func Create() options.OptParser {
+	return parser{}
+}
+
+type parser struct {}
+
+func (p parser) Parse(opts []string) ([]options.Opt, error) {
+    res := []options.Opt{}
+
+    folder := opts[0]
+
+    res = append(res, options.Opt{
+    	Name:  "",
+    	Value: folder,
+    })
+
+	return res, nil
+}
--- args/parser/slideshowopts/slideshowopts_test.go
+++ args/parser/slideshowopts/slideshowopts_test.go
@@ -1,3 +1,70 @@
 package slideshowopts_test
 
+import (
+	"errors"
+	"reflect"
+	"testing"
 
+	"github.com/VPavliashvili/wallpainter-go/args/parser/slideshowopts"
+	"github.com/VPavliashvili/wallpainter-go/domain"
+	"github.com/VPavliashvili/wallpainter-go/domain/opts"
+)
+
+func TestWhenOnlyOneArgument(t *testing.T) {
+	cases := []struct {
+		opts []string
+		want []opts.Opt
+	}{
+		{
+			opts: []string{"/path/to/some/folder/"},
+			want: []opts.Opt{
+				{
+					Name:  "",
+					Value: "/path/to/some/folder/",
+				},
+			},
+		},
+	}
+
+	parser := slideshowopts.Create()
+	for _, item := range cases {
+		got, err := parser.Parse(item.opts)
+		want := item.want
+
+		if err != nil {
+			t.Errorf("error should be nil, got -> %v", err)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("error in slideshow opts parsing\ngot\n%v\nwant\n%v", got, want)
+		}
+
+	}
+}
+
+func TestWhenOnlyOneArgButError(t *testing.T) {
+	cases := []struct {
+		opts []string
+		err  error
+	}{
+		{ //this checks not for certainly if folder exists but if string looks like folder path
+			opts: []string{"not a folder path"},
+			err: domain.InvalidPathError{
+				Path: "not a folder path",
+			},
+		},
+	}
+
+	parser := slideshowopts.Create()
+	for _, item := range cases {
+		res, err := parser.Parse(item.opts)
+		want := item.err
+
+		if res != nil {
+			t.Errorf("result should be nil in this case, got -> %v", res)
+		}
+		if !errors.Is(err, want) {
+			t.Errorf("should have thrown an error\ngot\n%v\nwant\n%v", err, want)
+		}
+	}
+}
