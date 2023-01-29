ecbd4b1f2c395d529f3b5a4f934de4c70db7ba26
VPavliashvili <v_pavliashvili@yahoo.com>
2023-01-29 12:01:35 +0400

    help text refactored out as html template file

--- cmds/slideshow/operations/helpbased/helpbased.go
+++ cmds/slideshow/operations/helpbased/helpbased.go
@@ -2,6 +2,8 @@ package helpbased
 
 import (
 	"fmt"
+	"html/template"
+	"os"
 
 	"github.com/VPavliashvili/wallpainter-go/cmds/slideshow/models"
 	"github.com/VPavliashvili/wallpainter-go/domain/cmds"
@@ -16,32 +18,46 @@ func Create(cmds.CmdArgument) models.Operation {
 type helpargument struct{}
 
 func (h helpargument) Execute() error {
-	msg := fmt.Sprintf(`
-            %v command has several options
+	relativePath := "./cmds/slideshow/operations/helpbased/text.tmpl"
+	tmpl, err := template.ParseFiles(relativePath)
 
-      1) run '%v %v' for this help menu
+	if err != nil {
+		return err
+	}
 
-      2) run '%v /images/containing/folder/' to simply run this command for all images from the directory
-            example: %v ~/Pictures/
+	data := struct {
+		Flag              string
+		HelpOpt           string
+		RecursiveOpt      string
+		ImagesOpt         string
+		TimeOpt           string
+		Minute            string
+		Second            string
+		TimeOptDefaultVal string
+		FehDefaultVal     string
+		FehValues         string
+		FehMax            string
+		FehCenter         string
+	}{
+		Flag:              data.Flag,
+		HelpOpt:           data.HelpOpt,
+		RecursiveOpt:      data.Recursiveopt,
+		ImagesOpt:         data.ImagesOpt,
+		TimeOpt:           data.TimeOpt,
+		Minute:            string(data.Minute),
+		Second:            string(data.Second),
+		TimeOptDefaultVal: fmt.Sprintf("%v", data.TimeoptDefaultVal),
+		FehDefaultVal:     feh.Scale,
+		FehValues:         feh.GetOptionAsString(),
+		FehMax:            feh.Max,
+		FehCenter:         feh.Center,
+	}
 
-      3) run '%v /folder/ %v' to run same command as above but recursively for all subfolders
-            example: %v ~/Pictures/ %v 
+	err = tmpl.Execute(os.Stdout, data)
 
-      4) run '%v %v [and specify distinct image file separated by space]'
-            example: %v %v ~/Pictures/image1.jpg ~/Pictures/otherpic.jpg
-         also feh specific scaling options can be used {%v}, default value is %v when ignored
-            example: %v %v ~/pic1.jpg %v ~/pic2.jpg %v
-        
-      5) '%v' option can be used for every combination of this command to specify time for next wallpaper change.
-         it expects value in minutes or seconds and can be passed int with prefix '%c' or '%c'. default value is %v minutes
-            example: %v /folder/ %v 10m (this means change folpapaer from /folder/ in every 10 minutes
-        `,
-		data.Flag, data.Flag, data.HelpOpt, data.Flag, data.Flag, data.Flag, data.Recursiveopt, data.Flag, data.Recursiveopt,
-		data.Flag, data.ImagesOpt, data.Flag, data.ImagesOpt, feh.GetOptionAsString(), feh.Scale,
-		data.Flag, data.ImagesOpt, feh.Center, feh.Max, data.TimeOpt,
-		data.Minute, data.Second,
-		data.TimeoptDefaultVal, data.Flag, data.TimeOpt)
+	if err != nil {
+		return err
+	}
 
-	fmt.Println(msg)
 	return nil
 }
